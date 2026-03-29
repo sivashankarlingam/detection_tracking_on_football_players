ae00b404435a82d92a9415060430878c52b0fae9
sivashankar <sivashankarlingam21@gmail.com>
2026-03-29 20:30:56 +0530

    Fix relation UndefinedTable and re-enable production migrations

--- admins/views.py
+++ admins/views.py
@@ -20,13 +20,8 @@ def AdminLoginCheck(request):
 
 
 def RegisterUsersView(request):
-    try:
-        data = list(UserRegistrationModel.objects.all())
-        return render(request, 'admins/viewregisterusers.html', context={'data': data})
-    except Exception as e:
-        from django.http import HttpResponse
-        import traceback
-        return HttpResponse(f"<h1>DEBUG TRACEBACK</h1><pre>{traceback.format_exc()}</pre>")
+    data = UserRegistrationModel.objects.all()
+    return render(request, 'admins/viewregisterusers.html', context={'data': data})
 
 
 
